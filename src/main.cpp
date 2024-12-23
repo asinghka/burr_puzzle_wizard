eed4d96f561d761ddf97ad3f4ba17934a8f27668
asinghka <singhkalra.arjun@gmail.com>
2024-12-23 02:05:07 +0100

    updated cmake file

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include "application.h"
 
-// const auto file_path = std::filesystem::path("E:\\Data\\Code\\masterarbeit-teufelsknoten-refactor\\res\\puzzles\\Puzzle6.txt");
 const auto file_path = std::filesystem::path("res/puzzles/Puzzle6.txt");
 
 int main(int argc, char** argv) {
